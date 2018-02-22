df4f69f47a8aa45bdc32065ba0918f69e6a535f2
Will Townes <will.townes@gmail.com>
2018-02-22 13:47:45 -0500

    minor change to finite mixture model

--- jags_test/finite_mixture.R
+++ jags_test/finite_mixture.R
@@ -8,8 +8,8 @@ mu2<-c(-10,0)
 mu3<-c(0,-10)
 mu_mat<-cbind(mu1,mu2,mu3)
 mu_grand<-c(20,-50)
-probs<-c(.5,.3,.2)
-z<-rmultinom(n,1,probs)
+probs_true<-probs<-c(.5,.3,.2)
+z_true<-z<-rmultinom(n,1,probs)
 #each row of ymn a data point, each column a dimension
 ymn<-t(mu_grand+mu_mat%*%z) 
 Sigma<-matrix(c(5,4,4,5),nrow=2)
@@ -34,7 +34,7 @@ model_string<- "model {
 
 jmod <- jags.model(textConnection(model_string), data = list(y=y,n=nrow(y),d=ncol(y),Nclust=3,Omega0=diag(2)), n.chains=3, quiet=TRUE)
 update(jmod, 100, progress.bar="none")
-SAMPS <- coda.samples(jmod, variable.names=c("mu_cl","z","probs","Omega"), n.iter=5000, progress.bar="none") #list of length n.chains
+system.time(SAMPS <- coda.samples(jmod, variable.names=c("mu_cl","z","probs","Omega"), n.iter=5000, progress.bar="none")) #list of length n.chains
 
 samps<-do.call("rbind",SAMPS)
 mu_cl<-samps[,grepl("mu_cl",colnames(samps))]
