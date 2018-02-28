2404b6f97b520362c4d169969bcd744253161085
Will Townes <will.townes@gmail.com>
2018-02-28 15:48:01 -0500

    clean up spike and slab regression and finite mixture examples

--- jags_test/finite_mixture.R
+++ jags_test/finite_mixture.R
@@ -15,29 +15,32 @@ ymn<-t(mu_grand+mu_mat%*%z)
 Sigma<-matrix(c(5,4,4,5),nrow=2)
 y<-t(sapply(1:n,function(n){mvrnorm(1,ymn[n,],Sigma)}))
 colnames(y)<-paste0("dim",1:2)
-ggplot(as.data.frame(y),aes(x=dim1,y=dim2))+geom_point()+geom_density2d()
+(baseplot<-ggplot(as.data.frame(y),aes(x=dim1,y=dim2))+geom_point()+geom_density2d())
 
-model_string<- "model {
-  # Likelihood:
-  for( i in 1 : n ) {
-    y[i,1:d] ~ dmnorm(mu[i,1:d], Omega)
-    mu[i,1:d] <- mu_cl[z[i], 1:d]
-    z[i] ~ dcat(probs)
-  }
-  # Prior:
-  Omega ~ dwish(Omega0, 3) #Sigma^{-1} prior
-  for ( k in 1: Nclust ) {
-    mu_cl[k,1:d] ~ dmnorm( c(0,0) , .0001*Omega0 )
-  }
-  probs ~ ddirch(c(1,1,1))
-}"
-
-jmod <- jags.model(textConnection(model_string), data = list(y=y,n=nrow(y),d=ncol(y),Nclust=3,Omega0=diag(2)), n.chains=3, quiet=TRUE)
-update(jmod, 100, progress.bar="none")
-system.time(SAMPS <- coda.samples(jmod, variable.names=c("mu_cl","z","probs","Omega"), n.iter=5000, progress.bar="none")) #list of length n.chains
-
-samps<-do.call("rbind",SAMPS)
+jmod<-jags.model("finite_mixture.bugs", data = list(y=y,n=nrow(y),d=ncol(y),Nclust=3,Omega0=diag(2)), n.chains=3, quiet=TRUE)
+update(jmod, 1000, progress.bar="none")
+system.time(samps0<-coda.samples(jmod, variable.names=c("mu_cl","z","probs","Omega"), n.iter=5000, progress.bar="none")) 
+#list of length n.chains
+#convert to data frame
+samps<-do.call("rbind",samps0)
 mu_cl<-samps[,grepl("mu_cl",colnames(samps))]
 z<-samps[,grepl("z",colnames(samps))]
 probs<-samps[,grepl("probs",colnames(samps))]
 Omega<-samps[,grepl("Omega",colnames(samps))]
+
+#check results invariant to label switching
+#covariance matrix
+solve(matrix(colMeans(Omega),nrow=2)) #5,4; 4,5
+#check results that are vulnerable to label switching
+#use a single sample from very end of chain
+tail(probs,1) #c(.5,.3,.2) order doesn't matter
+tail(mu_cl,1)
+s<-sample.int(nrow(mu_cl),100)
+f<-function(k){
+  m<-as.data.frame(mu_cl[s,c(k,k+3)])
+  colnames(m)<-c("dim1","dim2")
+  m$k<-paste0("clust",k)
+  m
+}
+mu_s<-do.call("rbind",lapply(1:3,f))
+baseplot+geom_point(aes(x=dim1,y=dim2),data=mu_s,colour="red",shape=2,size=2)
--- jags_test/spike_slab_regression.R
+++ jags_test/spike_slab_regression.R
@@ -11,35 +11,17 @@ y<-drop(mu_true+X%*%beta_true+rnorm(N,sd=sy))
 summary(lm(y~X))
 
 #jags part
-model_string <- "model{
-  # Likelihood
-  for(i in 1:N){
-    #variance=1/tau_y
-    y[i] ~ dnorm(eta[i],tau_y) 
-    #eta[i] <- mu + inprod(beta[],X[i,])
-  }
-  eta<-mu+X%*%beta
+#step 1 compile the jags model from the specification file
+jmod <- jags.model("spike_slab_regression.bugs", data = list(y=y,N=N,X=X,k=k), n.chains=3, quiet=TRUE)
+#step 2 initialize all variables and run for the burn-in period
+update(jmod, 1000, progress.bar="none")
+#step 3 sample from the model and store in a convenient coda object
+samps0<-coda.samples(jmod, variable.names=c("beta","delta"), n.iter=5000, progress.bar="none") #list of length n.chains
+traceplot(samps0)
 
-  #Priors
-  for(j in 1:k){
-    gamma[j] ~ dnorm(0,tau_gam)
-    delta[j] ~ dbern(omega)
-    beta[j]  <- gamma[j]*delta[j]
-  }
-
-  omega ~ dunif(0,1)
-  tau_gam ~ dgamma(.1,.1)
-  tau_y ~ dgamma(.001,.001)
-  mu ~ dt(0,0.001,4)
-}"
-
-SSVS <- jags.model(textConnection(model_string), data = list(y=y,N=N,X=X,k=k), n.chains=3, quiet=TRUE)
-update(SSVS, 10000, progress.bar="none")
-SAMPS <- coda.samples(SSVS, variable.names=c("beta","delta"), n.iter=50000, progress.bar="none") #list of length n.chains
-
-samps<-do.call("rbind",SAMPS)
+samps<-do.call("rbind",samps0)
 beta   <- samps[,1:k]
-delta  <- samps[,1:k+k]
+delta  <- samps[,(k+1):(2*k)]
 colnames(beta)<-colnames(delta)<- colnames(X)
 boxplot(beta,outline=FALSE)
 colMeans(delta)
